389f012daacae38d47801742d2dd8f75d7ab56e2
Wan <139746923+wanpc4@users.noreply.github.com>
2023-09-02 08:34:30 +0800

    Create enter_age.py
    My first Python coding

---
+++ enter_age.py
@@ -0,0 +1,16 @@
+#Hello, this is my first python coding
+#python is considered one of the best and easier programming language to learn 
+#This is the most basic Python I've ever made other than print("hello world!") XD
+
+x = int(input("Enter age "))
+
+y = input("Do you feel old enough? y/n ")
+
+if x >= 30:
+    print("You are ",x," years old")
+    print("You're getting older.")
+elif x <= 29:
+    print("You are ",x," years old")
+    print("You're still young.")
+else: 
+    print("Invalid input")
